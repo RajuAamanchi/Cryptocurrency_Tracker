0e8f9b385a3ca89fad4fe44d7cc848ced7f4b480
RajuAamanchi <rajuaamanchi24@gmail.com>
2025-07-19 21:34:18 -0500

    Initial commit

---
+++ Cryptocurrency-tracker-dashboard/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CryptocurrencyTracker from './components/CryptocurrencyTracker'
+
+const App = () => (
+  <div>
+    <CryptocurrencyTracker />
+  </div>
+)
+
+export default App
---
+++ Cryptocurrency-tracker-dashboard/src/components/CryptocurrencyTracker/index.js
@@ -0,0 +1,54 @@
+import {Component} from 'react'
+import Loader from '../Loader'
+
+import CryptocurrenciesList from '../CryptocurrenciesList'
+
+import './index.css'
+
+const apiUrl = 'https://apis.ccbp.in/crypto-currency-converter'
+
+class CryptocurrencyTracker extends Component {
+  state = {
+    cryptocurrenciesData: [],
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    this.getCryptocurrencies()
+  }
+
+  getCryptocurrencies = async () => {
+    const response = await fetch(apiUrl)
+    const fetchedData = await response.json()
+
+    this.setState({
+      cryptocurrenciesData: fetchedData.map(eachCryptocurrency => ({
+        id: eachCryptocurrency.id,
+        currencyLogoUrl: eachCryptocurrency.currency_logo,
+        currencyName: eachCryptocurrency.currency_name,
+        usdValue: eachCryptocurrency.usd_value,
+        euroValue: eachCryptocurrency.euro_value,
+      })),
+      isLoading: false,
+    })
+  }
+
+  renderCryptocurrenciesList = () => {
+    const {cryptocurrenciesData} = this.state
+
+    return <CryptocurrenciesList cryptocurrenciesData={cryptocurrenciesData} />
+  }
+
+
+  render() {
+    const {isLoading} = this.state
+
+    return (
+      <div className="app-container">
+        {isLoading ? <Loader /> : this.renderCryptocurrenciesList()}
+      </div>
+    )
+  }
+}
+
+export default CryptocurrencyTracker
